990d3148d7684e3e06e614a8ca6f7975d1ab4ac7
jose sanchez <sanchez24jose@gmail.com>
2018-04-11 19:24:48 -0500

    added key up function

--- js/app.js
+++ js/app.js
@@ -3,16 +3,21 @@ $(document).ready(function(){
     
     $('.ryu').mouseenter(function(){
         $('.ryu-still').hide();
+		$('.ryu-throwing').hide();
+		$('.ryu-cool').hide();
         $('.ryu-ready').show();
     })
-    .mouseleave(function() {
-        $('.ryu-still').show();
+	.mouseleave(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-cool').hide();
         $('.ryu-ready').hide();
-        
-    })
-    .mousedown(function(){
+		$('.ryu-still').show();
+	})
+	.mousedown(function(){
         playHadouken();
         $('.ryu-ready').hide();
+		$('.ryu-still').hide();
+		$('.ryu-cool').hide();
         $('.ryu-throwing').show();
         $('.hadouken').finish().show().animate(
             {'left': '1020px'},
@@ -24,18 +29,29 @@ $(document).ready(function(){
     })
     .mouseup(function(){
         $('.ryu-throwing').hide();
+		$('.ryu-still').hide();
+		$('.ryu-cool').hide();
         $('.ryu-ready').show();
+		
     })
     
-  $(document).on('keydown', function(key) {
-    if (key.keyCode === 88) {
-      $('.ryu-still').hide();
-      $('.ryu-ready').hide();
-      $('.ryu-cool').show();
-    }
-});
+	$(document).on('keydown', function(key) {
+		if (key.keyCode === 88) {
+		  $('.ryu-still').hide();
+		  $('.ryu-throwing').hide();
+		  $('.ryu-ready').hide();
+		  $('.ryu-cool').show();
+		}
+	});
+	
+	$(document).on('keyup', function() {
+		$('.ryu-cool').hide();
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	});
 
-});
+}); // End of .ready()
 
     
     function playHadouken() {
